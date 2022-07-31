4b08290f631674bf1e5e7341bd745253a3896dd2
HuynhTanMao <maotan1302@gmail.com>
2022-07-31 08:16:37 +0700

    change text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Tan Mao
+          Tan Maos
         </p>
       </header>
     </div>
