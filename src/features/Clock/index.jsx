87806970b98b21de4158863af6ca296961a268ca
HuynhTanMao <maotan1302@gmail.com>
2022-08-11 11:12:36 +0700

    Add custom hooks "useClock" trong ReactJS

--- src/features/Clock/index.jsx
+++ src/features/Clock/index.jsx
@@ -1,29 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import useClock from '../../hooks/useClock';
 
-function formatData() {
-    const date = new Date();
-    const hours = `0${date.getHours()}`.slice(-2);
-    const minutes = `0${date.getMinutes()}`.slice(-2);
-    const second = `0${date.getSeconds()}`.slice(-2);
-    return `${hours}:${minutes}:${second}`;
-}
+function Clock() {
 
-function Clock(props) {
-
-    const [timeString, setTimeString] = useState('');
-
-    useEffect(() => {
-        const clockInterval = setInterval(() => {
-            const now = new Date();
-            const newTimeString = formatData(now);
-            setTimeString(newTimeString);
-        }, 1000);
-        return () => {
-            console.log('Clock cleanup');
-            clearInterval(clockInterval);
-        }
-    }, []);
-
+    const { timeString } = useClock();
     return (
         <div>
             <p style={{ fontSize: '42px' }}>{timeString}</p>
---
+++ src/hooks/useClock.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+
+function formatData() {
+    const date = new Date();
+    const hours = `0${date.getHours()}`.slice(-2);
+    const minutes = `0${date.getMinutes()}`.slice(-2);
+    const second = `0${date.getSeconds()}`.slice(-2);
+    return `${hours}:${minutes}:${second}`;
+}
+
+function useClock(props) {
+
+    const [timeString, setTimeString] = useState('');
+
+    useEffect(() => {
+        const clockInterval = setInterval(() => {
+            const now = new Date();
+            const newTimeString = formatData(now);
+            setTimeString(newTimeString);
+        }, 1000);
+        return () => {
+            console.log('Clock cleanup');
+            clearInterval(clockInterval);
+        }
+    }, []);
+
+    return ({ timeString });
+}
+
+export default useClock;
