aeccbb42bce6bde236500ed8c4cc35b476bb9e4b
HuynhTanMao <maotan1302@gmail.com>
2022-08-15 15:20:18 +0700

    Move old feature to HomePage Component

--- src/hooks/useClock.js
+++ src/hooks/useClock.js
@@ -1,8 +1,7 @@
 import { useEffect, useState } from 'react';
 
-function formatData() {
-    const date = new Date();
-    const hours = `0${date.getHours()}`.slice(-2);
+function frmatData(date) {
+    const hoours = `0${date.getHours()}`.slice(-2);
     const minutes = `0${date.getMinutes()}`.slice(-2);
     const second = `0${date.getSeconds()}`.slice(-2);
     return `${hours}:${minutes}:${second}`;
